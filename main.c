60961b0d8861fed4cfc79bde4d202cf67fd08177
kevinstana <122367928+kevinstana@users.noreply.github.com>
2024-01-29 18:27:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,24 +5,11 @@
 #include <unistd.h>
 #include "students.h"
 
-// gcc -c students.c
-// gcc main.c students.o -o students
-
-// Μέθοδος για έλεγχο ονόματος που εισάγει ο χρήστης
 int validateNameInput(char *nameArray) {
     int isInputTooLong = 0;
     int spaceCounter = 0;
     int firstSpacePosition = 0;
 
-    /*  
-        Η fgets αποθηκεύει στο s.name έως 19 χαρακτήρες και στο 20ο βάζει το '\0'.
-        Αν η είσοδος είναι μικρότερη από 19 χαρακτήρες, αποθηκεύεται και το '\n', 
-        οπότε αυτό αντικαθιστάται με το '\0'. Σε περίπτωση εισόδου μεγαλύτερης 
-        των 19 χαρακτήρων, δηλαδή αν δεν υπάρχει το '\n', αδειάζει το buffer του input.
-        (εδώ βασικά και σωστό μέγιστο μέγεθος να δώσει ο χρήστης, πάλι θα πάει στο else 
-        γιατί η συνθήκη για να μπει στο else είναι απλά να μην είναι '\n' το στοιχείο πριν το '\0'.
-        Όμως σε σωστή μέγιστη είσοδο δεν θα πάρει την τιμή 1 το isTooLong.)
-    */
     if (nameArray[strlen(nameArray) - 1] == '\n') {
         nameArray[strlen(nameArray) - 1] = '\0';
     } else {
@@ -33,7 +20,7 @@ int validateNameInput(char *nameArray) {
     }
 
     if (strlen(nameArray) == 1 && nameArray[0] == '0') {
-        return 2; // 2 σημαίνει ότι έδωσε 0 για είσοδο άρα θέλει να επιστρέψει στο menu
+        return 2;
     }
 
     if (strlen(nameArray) < 5) {
@@ -98,12 +85,10 @@ int validateNameInput(char *nameArray) {
     return TRUE;
 }
 
-// Μέθοδος για έλεγχο του ID που εισάγει ο χρήστης
 int validateIdInput(unsigned long *id, char *inputArray) {
     int isInputTooLong = 0;
     int isNotNumber;
 
-    // Έλεγχος αν ο χρήστης έδωσε λιγότερους από 20 χαρακτήρες και αντικατάσταση του '\n' με '\0' αν αυτό συνέβη
     if (inputArray[strlen(inputArray) - 1] == '\n') {
         inputArray[strlen(inputArray) - 1] = '\0';
     } else {
@@ -113,14 +98,11 @@ int validateIdInput(unsigned long *id, char *inputArray) {
         }
     }
 
-    // Tο inputArray χωράει 20 χαρακτήρες (χωρίς το '\0'). Αν το σύστημα είναι 32bit το μέγιστο unsigned long έχει 10 ψηφία
-    // οπότε αν το inputArray έχει πάνω από 10 χαρακτήρες τότε επιστρέφεται μήνυμα λάθους
     if ( (sizeof(unsigned long) == 4) && (strlen(inputArray) > 10) ) {
         printf("Input too long. Try again.\n");
         return FALSE;
     }
-    
-    // Έλεγχος για μεγάλη είσοδο σε 64bit σύστημα
+
     if (isInputTooLong) {
         printf("Input too long. Try again.\n");
         return FALSE;
@@ -131,7 +113,6 @@ int validateIdInput(unsigned long *id, char *inputArray) {
         return FALSE;
     }
 
-    // Έλεγχος για μόνο αριθμούς
     int i = -1;
     isNotNumber = 0;
     while (inputArray[++i] != '\0') {
@@ -142,14 +123,8 @@ int validateIdInput(unsigned long *id, char *inputArray) {
         }
     }
 
-    /*
-        Η είσοδος του χρήστη για το id αρχικά αποθηκεύεται σε string. Για να μην γίνει μετατροπή
-        σε unsigned long μιας τιμής μεγαλύτερης από αυτήν που μπορεί να κρατήσει το σύστημα
-        αποθηκεύεται η μέγιστη τιμή unsigned long του συστήματος σαν string. Μετά γίνεται σύγκριση 
-        αν το max value είναι μικρότερο από το id που έδωσε ο χρήστης. 
-    */
     if ((sizeof(unsigned long) == 8) && (strlen(inputArray) == 20)) {
-        char maxUL_64bit[] = {"18446744073709551615"}; // max unsigned long για 64bit
+        char maxUL_64bit[] = {"18446744073709551615"}; //
 
             if ( (strcmp(maxUL_64bit, inputArray)) < 0) {
                 printf("The input is too massive to be stored.\n");
@@ -157,7 +132,7 @@ int validateIdInput(unsigned long *id, char *inputArray) {
             }
 
     } else if ((sizeof(unsigned long) == 4) && (strlen(inputArray) == 10)) {
-        char maxUL_32bit[] = {"4294967295"}; // max unsigned long για 32bit
+        char maxUL_32bit[] = {"4294967295"};
 
         if ( (strcmp(maxUL_32bit, inputArray)) < 0) {
                 printf("The input is too massive to be stored.\n");
@@ -165,8 +140,8 @@ int validateIdInput(unsigned long *id, char *inputArray) {
             }
     }
 
-    char *endPtr = &inputArray[strlen(inputArray) + 1]; // Σε αυτό το στοιχείο είναι το '\0'
-    *id = strtoul(inputArray, &endPtr, 10); // Μετατροπή του string στο inputArray σε unsigned long. Το 10 είναι για το δεκαδικο σύστημα
+    char *endPtr = &inputArray[strlen(inputArray) + 1];
+    *id = strtoul(inputArray, &endPtr, 10);
 
     return TRUE;
 }
@@ -185,8 +160,7 @@ int main(int argc, char *argv[]) {
 
     switch (argc) {
         case 2:
-            // Έλεγχος αν υπάρχει το αρχείο
-            if (access(argv[1], F_OK) == 0) { // https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
+            if (access(argv[1], F_OK) == 0) {
                 printf("Opening %s...\n", argv[1]);
                 
                 result = load(argv[1], &studentList);
@@ -233,9 +207,7 @@ int main(int argc, char *argv[]) {
     int run = 1;
     char option;
     unsigned long id;
-    char idStr[21]; // Εδώ(idStr) αποθηκεύεται το id που εισάγει ο χρήστης, για έλεγχο πριν την μετατροπή σε unsigned long.
-                    // Ο πίνακας έχει 21 θέσεις γιατί για 64bit η μέγιστη τιμή unsigned long έχει 20 ψηφία, άρα το 
-                    // 21ο είναι για το '\0'. Αν το σύστημα είναι 32bit τα ψηφία θα είναι 10, άρα < 20 οπότε θα χωράει εδώ.
+    char idStr[21];
 
     while (run) {
         printf("\n");
@@ -250,13 +222,6 @@ int main(int argc, char *argv[]) {
             printf("Answer: ");
 			scanf(" %c", &option);   
             
-            /*
-                Το while αυτό το πήρα από το ChatGPT. Είναι για να αδειάζει το input buffer.
-                Αν έγραφα πχ. asdfasdf θα έπαιρνε σαν πρώτο input το a και στις επόμενες επαναλήψεις
-                της scanf θα συνέχιζε με τη σειρά το input, δηλαδή s μετά d, μετά f κλπ. 
-                Έτσι δηλαδή κρατάει μόνο το 1ο χαρακτήρα και "πετάει" τους υπόλοιπους. 
-                Επίσης έτσι μπορώ να μετρήσω και τους χαρακρήρες εισόδου μέσα στο while.
-            */
             int c;
             while ((c = getchar()) != '\n' && c != EOF) {
                 inputCounter++;
@@ -283,11 +248,9 @@ int main(int argc, char *argv[]) {
                 int run1 = 1;
                 while (run1) {
                     printf("Student's full name: ");
-                    fgets(s->name, sizeof(s->name), stdin); // fgets για να παίρνει string από τον χρήστη https://stackoverflow.com/questions/21691843/how-to-correctly-input-a-string-in-c
-
+                    fgets(s->name, sizeof(s->name), stdin);
                     int isValidName = validateNameInput(s->name);
 
-                    // ξεχώρισα τα if γιατί πολλά if, else εμφωλευμένα με μπέρδευαν
                     if (isValidName == FALSE) {
                         continue;
                     } 
@@ -307,7 +270,7 @@ int main(int argc, char *argv[]) {
                         }
                     }  
                     
-                    if (isValidName == 2) { // Αν ο χρήστης έδωσε 0, επιστροφή στο menu
+                    if (isValidName == 2) {
                         run1 = 0;
                     }
                 }
@@ -332,7 +295,7 @@ int main(int argc, char *argv[]) {
                         continue;
                     }
 
-                    if (id == 0) { // επιστροφή στο menu
+                    if (id == 0) {
                         run2 = 0;
                         continue;
                     }
@@ -340,8 +303,7 @@ int main(int argc, char *argv[]) {
                     result = deleteStudentById(id, studentList);
                     if (isError(result)) {
                         switch (result) {
-                            case SYNTAX_ERR: // Εδώ έβαλα SYNTAX_ERR εννοώντας λάθος στο id, με το σκεπτικό ότι 
-                                             // αν υπήρχε syntax error στον κώδικα δεν θα έκανε compile
+                            case SYNTAX_ERR:
                                 printf("Could not find the student with ID: %lu.\n", id);
                                 run2 = 0;
                                 break;
@@ -476,7 +438,6 @@ int main(int argc, char *argv[]) {
                         }
                     } 
                     
-                    // Αν ο χρήστης εισάγει 0 για επιστροφή στο menu
                     if (isValidName == 2) {
                         run4_b = 0;
                     }
@@ -507,7 +468,7 @@ int main(int argc, char *argv[]) {
                     }
 
                     if ( (exitOption != 'y' && exitOption != 'n') || exitInputCounter != 1 ) {
-                            printf("I'm silly. I understand only 'y' for yes and 'n' for no :(\n");
+                            printf("'y' for yes and 'n' for no :(\n");
                             continue;
                     }
 
